53e3c18fa60d0e32c1f7869f31dbc62fe8621123
Harol <hchaverra26@gmail.com>
2024-07-31 21:37:36 -0500

    pagina principal de todo-task

--- To-Do List/src/App.jsx
+++ To-Do List/src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
 
+import './gen/app.css'
+
 function App() {
 
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <div className='container-app'>
+          <div className='sup-barr'>
+             <h1>To-Do List  <i class="fa-solid fa-list-check"></i></h1>
+          </div>
+          <div className='container-task event'>
+
+
+
+          </div>
+          
+           <div className='container-task event'>
+
+
+            
+          </div>
+          
+      </div>
+      <div className='box-rell'></div>
+
+      <div className='botton-add'>
+              Añadir Tarea
+      </div>
     </>
   )
 }
