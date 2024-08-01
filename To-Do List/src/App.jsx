c573df664c986a4ef4f97acd6e624ed7058228dc
Harol <hchaverra26@gmail.com>
2024-07-31 20:33:50 -0500

    organizacion de proyecto

---
+++ To-Do List/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hola mundo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ To-Do List/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+//css import general style for component
+import './gen/styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
