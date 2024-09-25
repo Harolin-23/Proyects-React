6f90777ea315e421f22eb4e78f4ef28fcb110b7b
Harol <hchaverra26@gmail.com>
2024-09-25 15:53:13 -0500

    Eliminar archivos y actualizar configuración de Vite y React

---
+++ React-Proyects/To-Do List/src/data/data.js
@@ -0,0 +1,11 @@
+export let data = [
+    {
+        "Title": "Ejemplo de Tarea",
+        "Description": "Deja apretado para Abrir el editor de las tareas,para eliminar de click en el circulo",
+        "Priority": "Alta",
+        "TimeFinal": "10:00:00",
+        "TimeAdded": "08:00:00"
+    },
+
+];
+
